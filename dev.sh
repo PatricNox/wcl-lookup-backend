fe395faa8d80f1cf17167d1a01d4f9bfaf57ca6f
patricucs <patric.johansson@ucsit.se>
2021-10-21 16:06:31 +0200

    Setup Laravel backend through Docker

---
+++ dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  USER="1000"
+else
+  USER="33"
+fi
+
+# Container
+bash() {
+  docker-compose exec --user $USER app bash
+}
+
+root() {
+  docker-compose exec --user root app bash
+}
+
+# Tools
+composer() {
+    docker-compose exec --user $USER app composer "$@"
+}
+
+npm() {
+    docker-compose exec --user $USER app npm "$@"
+}
+
+# Laravel
+artisan() {
+    docker-compose exec --user $USER app php artisan "$@"
+}
+
+
+"$@"
