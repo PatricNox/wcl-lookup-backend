8cab012ae74d5ef3d9682e202a833793ffd39d86
patricnox <hello@patricnox.info>
2021-10-23 13:49:37 +0200

    Have Laravel routes base on default namespace

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -8,6 +8,15 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
      * The path to the "home" route for your application.
      *
      * @var string
@@ -64,6 +73,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('api')
             ->middleware('api')
+            ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
 }
